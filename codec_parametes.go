8e581a9a2a1ac6e471e33eaea9aaeeaac2bf4c70
Alexey Dogonin <alex.dogonin@gmail.com>
2020-07-24 01:23:50 +0300

    add methods to CodecParameters

--- codec_parametes.go
+++ codec_parametes.go
@@ -6,6 +6,30 @@ import "unsafe"
 
 type CodecParameters C.struct_AVCodecParameters
 
+func (c *CodecParameters) CodecID() CodecID {
+	return CodecID(c.ctype().codec_id)
+}
+
+func (c *CodecParameters) CodecType() MediaType {
+	return MediaType(c.ctype().codec_type)
+}
+
+func (c *CodecParameters) Width() int {
+	return int(c.ctype().width)
+}
+
+func (c *CodecParameters) Height() int {
+	return int(c.ctype().height)
+}
+
+func (c *CodecParameters) Format() int {
+	return int(c.ctype().format)
+}
+
+func (c *CodecParameters) Bitrate() int {
+	return int(c.ctype().bit_rate)
+}
+
 func (parms *CodecParameters) ctype() *C.struct_AVCodecParameters {
 	return (*C.struct_AVCodecParameters)(unsafe.Pointer(parms))
 }

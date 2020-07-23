cfac9a1ca110d62c8a803059196ba9db6be0d0bb
Alexey Dogonin <alex.dogonin@gmail.com>
2020-07-24 01:26:52 +0300

    change base type of pixel format

--- pixel_format.go
+++ pixel_format.go
@@ -10,7 +10,7 @@ const (
 	YUV444P  PixelFormat = C.AV_PIX_FMT_YUV444P
 )
 
-type PixelFormat C.enum_AVPixelFormat
+type PixelFormat int //C.enum_AVPixelFormat
 
 func (fmt PixelFormat) ctype() C.enum_AVPixelFormat {
 	return (C.enum_AVPixelFormat)(fmt)

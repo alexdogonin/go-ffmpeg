41043c54b25f050a21fd4ac66e49fa1e25ebca16
Alexey Dogonin <alex.dogonin@gmail.com>
2020-07-24 01:27:19 +0300

    demuxing example in progress...

--- examples/demuxing/main.go
+++ examples/demuxing/main.go
@@ -32,5 +32,57 @@ func main() {
 		log.Fatal("streams not found")
 	}
 
-	
+	streams := formatContext.Streams()
+
+	var videoStream, audioStream *ffmpeg.Stream
+	for _, s := range streams {
+		if videoStream != nil && audioStream != nil {
+			break
+		}
+
+		p := s.CodecParameters()
+
+		if audioStream == nil && p.CodecType() == ffmpeg.MediaTypeAudio {
+			audioStream = s
+			continue
+		}
+
+		if videoStream == nil && p.CodecType() == ffmpeg.MediaTypeVideo {
+			videoStream = s
+			continue
+		}
+	}
+
+	if audioStream == nil {
+		log.Fatal("audio stream not found")
+	}
+
+	if videoStream == nil {
+		log.Fatal("video stream not found")
+	}
+
+	videoCodec, err := ffmpeg.CodecByID(videoStream.CodecParameters().CodecID())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	audioCodec, err := ffmpeg.CodecByID(audioStream.CodecParameters().CodecID())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	codecParms := videoStream.CodecParameters()
+	videoCodecContext, err := ffmpeg.NewVideoCodecContext(
+		videoCodec,
+		codecParms.Width(),
+		codecParms.Height(),
+		ffmpeg.PixelFormat(codecParms.Format()),
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	codecParms = audioStream.CodecParameters()
+	audioCodecContext, err := ffmpeg.NewAudioCodecContext(audioCodec, )
 }

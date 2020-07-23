b2873ac85e91e357f8e7d35fe1547e7b99f1567c
Alexey Dogonin <alex.dogonin@gmail.com>
2020-07-24 01:23:03 +0300

    add media type

---
+++ media_type.go
@@ -0,0 +1,16 @@
+package ffmpeg
+
+//#include <libavutil/avutil.h>
+import "C"
+
+const (
+	MediaTypeUnknown    = MediaType(C.AVMEDIA_TYPE_UNKNOWN)
+	MediaTypeVideo      = MediaType(C.AVMEDIA_TYPE_VIDEO)
+	MediaTypeAudio      = MediaType(C.AVMEDIA_TYPE_AUDIO)
+	MediaTypeData       = MediaType(C.AVMEDIA_TYPE_DATA)
+	MediaTypeSubtitle   = MediaType(C.AVMEDIA_TYPE_SUBTITLE)
+	MediaTypeAttachment = MediaType(C.AVMEDIA_TYPE_ATTACHMENT)
+	MediaTypeNB         = MediaType(C.AVMEDIA_TYPE_NB)
+)
+
+type MediaType C.enum_AVMediaType
